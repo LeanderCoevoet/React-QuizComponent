5d16dd11bde11a51bfc170b745ff666432c8d0de
BelgianCrowbar <leandercoevoet1@gmail.com>
2020-03-25 14:41:06 +0100

    Module3

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
+import QuizQuestionButton from "./QuizQuestionButton.js"
 
 class QuizQuestion extends Component {
     render() {
         return(
+            <div>
         <main>
             <section>
                 <p>{this.props.quiz_question.instruction_text}</p>
             </section>
             <section className="buttons">
-            <ul>
-                <li>{this.props.quiz_question.answer_options[0]}</li>
-            </ul>
+                <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
             </section>
         </main>
+        </div>
         )
     }
 }
